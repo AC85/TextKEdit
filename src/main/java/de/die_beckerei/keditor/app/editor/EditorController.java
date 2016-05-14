d802b4c93fbf6e5c439a10653a59a676f04d58ec
Flo <flo@die-beckerei.de>
2016-05-15 00:11:03 +0200

    - Initializable is deprecated (more or less) - load editor within editor area as default

---
+++ src/main/java/de/die_beckerei/keditor/app/editor/EditorController.java
@@ -0,0 +1,20 @@
+package de.die_beckerei.keditor.app.editor;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Flo on 14.05.16.
+ */
+public class EditorController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private void initialize() {
+
+    }
+}
--- src/main/java/de/die_beckerei/keditor/app/mainAppController.java
+++ src/main/java/de/die_beckerei/keditor/app/mainAppController.java
@@ -1,17 +1,29 @@
 package de.die_beckerei.keditor.app;
 
+import de.die_beckerei.keditor.app.editor.EditorController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Created by Flo on 14.05.16.
  */
-public class MainAppController implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+public class MainAppController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    Pane editorArea;
+
+    @FXML
+    private void initialize() {
+        this.initEdtior();
     }
 
     public void onOpen() {
@@ -27,6 +39,17 @@ public class MainAppController implements Initializable {
     }
 
     public void onSaveAs() {
+
+    }
+
+    private void initEdtior() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("editor/editor.fxml"));
+
+        try {
+            this.editorArea.getChildren().add(loader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
