d1d082da46102ed1a1888268c0fe6c8793a1ab68
Flo <flo@die-beckerei.de>
2016-05-15 02:12:32 +0200

    new editor tabs can be created easily

--- src/main/java/de/die_beckerei/keditor/app/editor/EditorController.java
+++ src/main/java/de/die_beckerei/keditor/app/editor/EditorController.java
@@ -2,6 +2,7 @@ package de.die_beckerei.keditor.app.editor;
 
 import de.die_beckerei.keditor.app.file.File;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 
 import java.net.URL;
@@ -19,10 +20,38 @@ public class EditorController {
     @FXML
     private TabPane tabbar;
 
+    @FXML
+    private Label newFile;
+
 
     @FXML
     private void initialize() {
+        Tab defaultTab = new Tab(new File());
 
-        this.tabbar.getTabs().add(new Tab(new File()));
+        this.addNewTab(defaultTab, true);
+    }
+
+
+    public void onNewFileTab() {
+        Tab tab = new Tab(new File());
+        this.addNewTab(tab, true);
+    }
+
+
+    /**
+     * adds a newTab at the last position, but before the "+"-Tab which should always be last
+     * @param newTab
+     */
+    private void addNewTab(Tab newTab, boolean selected) {
+        int tabbarSize = this.tabbar.getTabs().size();
+        if(tabbarSize <= 0) {
+            this.tabbar.getTabs().add(newTab);
+        } else {
+            this.tabbar.getTabs().add(tabbarSize - 1, newTab);
+        }
+
+        if(selected) {
+            this.tabbar.getSelectionModel().select(newTab);
+        }
     }
 }
