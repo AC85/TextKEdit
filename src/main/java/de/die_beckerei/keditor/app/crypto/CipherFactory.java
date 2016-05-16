24debacee149f65ea8f96b19f71a3827064b1a39
Flo <flo@die-beckerei.de>
2016-05-17 01:35:54 +0200

    cipherFactory can return AES oder DES by now

--- src/main/java/de/die_beckerei/keditor/app/crypto/CipherFactory.java
+++ src/main/java/de/die_beckerei/keditor/app/crypto/CipherFactory.java
@@ -1,15 +1,29 @@
 package de.die_beckerei.keditor.app.crypto;
 
-
+import de.die_beckerei.keditor.app.crypto.cipher.AES;
 import de.die_beckerei.keditor.app.crypto.cipher.Cipher;
+import de.die_beckerei.keditor.app.crypto.cipher.DES;
+import de.die_beckerei.keditor.app.crypto.cipher.types.AlgoAES;
+import de.die_beckerei.keditor.app.crypto.cipher.types.AlgoDES;
 
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
 /**
  *
  */
 public class CipherFactory {
 
-    public static Cipher getInstance() {
-        return null;
+    public static Cipher getInstance(Cipher.TYPE ciphertype) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
+
+        switch (ciphertype) {
+            case AES:
+                return new AES(new AlgoAES());
+            case DES:
+                return new DES(new AlgoDES());
+            default:
+                return null;
+        }
     }
 }
