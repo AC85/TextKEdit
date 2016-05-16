c7e97cd2955fb13fafb1884b33db3d59fc1eebaa
Flo <flo@die-beckerei.de>
2016-05-17 01:31:07 +0200

    algorithms now throw exceptions

--- src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/Algorithm.java
+++ src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/Algorithm.java
@@ -4,6 +4,6 @@ package de.die_beckerei.keditor.app.crypto.cipher.types;
  * Created by Flo on 16.05.16.
  */
 public interface Algorithm {
-    byte[] encrypt(byte[] contex);
-    byte[] decrypt(byte[] contex);
+    byte[] encrypt(byte[] contex) throws Exception;
+    byte[] decrypt(byte[] contex) throws Exception;
 }
