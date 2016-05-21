b1d17840fa97a73d233e608c91346177ed88727e
Flo <flo@die-beckerei.de>
2016-05-21 23:50:11 +0200

    fixed typo

--- src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/Algorithm.java
+++ src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/Algorithm.java
@@ -4,6 +4,6 @@ package de.die_beckerei.keditor.app.crypto.cipher.types;
  * Created by Flo on 16.05.16.
  */
 public interface Algorithm {
-    byte[] encrypt(byte[] contex) throws Exception;
-    byte[] decrypt(byte[] contex) throws Exception;
+    byte[] encrypt(byte[] context) throws Exception;
+    byte[] decrypt(byte[] context) throws Exception;
 }
