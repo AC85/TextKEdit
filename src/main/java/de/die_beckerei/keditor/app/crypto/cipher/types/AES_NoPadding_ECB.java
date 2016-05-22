b07aee73860358d322f1936bf2e7cca22f4f4bfd
Flo <flo@die-beckerei.de>
2016-05-22 14:07:49 +0200

    use default key

--- src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/AES_NoPadding_ECB.java
+++ src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/AES_NoPadding_ECB.java
@@ -16,8 +16,15 @@ public class AES_NoPadding_ECB implements Algorithm {
 
     public AES_NoPadding_ECB(CipherSettings settings) throws Exception {
 
+        //settings.getKey().getBytes(Document.charset)
+        //TODO generate appropriate key
+        byte[] keyBytes = new byte[] {
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
+
         this.cipher = Cipher.getInstance("AES/ECB/NoPadding", settings.getProvider());
-        this.key = new SecretKeySpec(settings.getKey().getBytes(Document.charset), "AES");
+        this.key = new SecretKeySpec(keyBytes, "AES");
 
     }
 
