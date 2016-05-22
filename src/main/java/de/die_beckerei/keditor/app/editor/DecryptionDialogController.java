103792560b83d5f1decd741eb21ad5db169a7cac
Flo <flo@die-beckerei.de>
2016-05-22 22:03:03 +0200

    create encryption dialog with dummy controller

---
+++ src/main/java/de/die_beckerei/keditor/app/editor/DecryptionDialogController.java
@@ -0,0 +1,21 @@
+package de.die_beckerei.keditor.app.editor;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by Flo on 22.05.16.
+ */
+public class DecryptionDialogController {
+
+    public void cancelDialog() {
+
+    }
+
+    public void decryptCurrent() {
+
+    }
+
+    public void decryptFile() {
+
+    }
+}
