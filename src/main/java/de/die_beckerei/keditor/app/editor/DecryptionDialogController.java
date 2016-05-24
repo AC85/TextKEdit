3517136c80f61fb4a04ad55432b9c5636890131c
Flo <flo@die-beckerei.de>
2016-05-24 11:21:31 +0200

    fixed encryption dialog ui -- added @fxml annotation for controller

--- src/main/java/de/die_beckerei/keditor/app/editor/DecryptionDialogController.java
+++ src/main/java/de/die_beckerei/keditor/app/editor/DecryptionDialogController.java
@@ -1,11 +1,27 @@
 package de.die_beckerei.keditor.app.editor;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Created by Flo on 22.05.16.
  */
 public class DecryptionDialogController {
+
+    @FXML
+    ComboBox DecryptCipherTyp;
+
+    @FXML
+    ComboBox DecryptComboPadding;
+
+    @FXML
+    ComboBox DecryptComboBlockMode;
+
+    @FXML
+    TextField DecryptPassword;
 
     public void cancelDialog() {
 
