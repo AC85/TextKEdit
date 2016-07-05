23442e39d5ba04beb600739eda3ad6842730ee7e
ac85 <ac85@me.com>
2016-07-05 17:33:31 +0200

    Fool teile gelöscht

--- src/main/java/de/die_beckerei/keditor/app/crypto/cipher/types/Algorithm.java
+++
@@ -1,9 +0,0 @@
-package de.die_beckerei.keditor.app.crypto.cipher.types;
-
-/**
- * Created by Flo on 16.05.16.
- */
-public interface Algorithm {
-    byte[] encrypt(byte[] context) throws Exception;
-    byte[] decrypt(byte[] context) throws Exception;
-}
